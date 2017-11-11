d89b7314e90b368cfce882888aaccb1456756e8a
brickatyourfeet <brickatyourfeet@gmail.com>
2017-11-10 18:51:02 -0800

    add user post auth

--- server/server.js
+++ server/server.js
@@ -114,6 +114,18 @@ app.post('/users', (req, res) => {
   })
 })
 
+app.get('users/me', (req, res) => {
+  let token = req.header('x-auth')
+
+  User.findByToken(token).then((user) => {
+    if (!user) {
+
+    }
+
+    res.send(user)
+  })
+})
+
 app.listen(port, () => {
   console.log(`running at port ${port}`)
 })
