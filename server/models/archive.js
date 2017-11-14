13163bdf36dc900a5049154ef803dd1c72bcf0b3
brickatyourfeet <brickatyourfeet@gmail.com>
2017-11-14 10:24:22 -0800

    add temp archive model

--- server/models/archive.js
+++ server/models/archive.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose')
+
+let Archive = mongoose.model('Archive', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  },
+  _createdBy: {
+    type: mongoose.Schema.Types.ObjectId
+  },
+  solvedBy: {
+    type: String
+  }
+})
+
+module.exports = { Archive }
